f47ea2155c4ec20db4154e0dc2cd0ce74a86c1cc
Teerawut Saesim <55015430+ribbinpo@users.noreply.github.com>
2024-03-14 23:44:39 +0700

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import "dotenv/config";
+
+import router from "./routes";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running :)");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running on ${port}`);
+});
